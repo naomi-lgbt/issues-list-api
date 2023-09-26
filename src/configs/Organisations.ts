d905c2c8910cdb8f174f5921304914423b1a4c14
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-26 16:44:53 -0700

    chore: remove deleted orgs

--- src/configs/Organisations.ts
+++ src/configs/Organisations.ts
@@ -3,8 +3,4 @@ import { AggregateData } from "../interfaces/AggregateData";
 export const orgList: (keyof Omit<AggregateData, "updatedAt">)[] = [
   "naomi-lgbt",
   "nhcarrigan",
-  "beccalyria",
-  "beccalia",
-  "rosalianightsong",
-  "naomis-novas",
 ];
